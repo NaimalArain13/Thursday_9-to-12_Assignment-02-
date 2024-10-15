6431093c42eccd09528670807d5aefc180aa3f48
NaimalArain13 <naimalarain13@gmail.com>
2024-10-15 11:26:36 +0500

    update banner

--- app/components/Hero-Section/Hero-Section.tsx
+++ app/components/Hero-Section/Hero-Section.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function HeroSection() {
   return (
-    <div className="mx-auto overflow-hidden bg-neutral-100 flex flex-col items-center pt-10 md:pt-20 min-h-screen space-y-4 md:space-y-6 text-center px-4">
+    <div className="mx-auto overflow-hidden bg-neutral-100 flex flex-col items-center pt-10 md:pt-20 min-h-screen space-y-7 md:space-y-9 text-center px-4">
       <h1 className="text-2xl md:text-6xl font-bold tracking-tight text-gray-900 leading-[1.5]">
         ENGAGE VISITORS.
         <br />
@@ -18,14 +18,14 @@ export default function HeroSection() {
         drives company growth, and improves customer satisfaction.
       </p>
       <div className="md:flex-row flex-col space-y-5  md:space-x-5 pt-6">
-        <Button className="bg-black text-lg text-white px-4 md:px-8 py-6 rounded-full shadow-md hover:bg-gray-800 transition duration-200">
+        <Button className="bg-black text-lg text-white px-4 md:px-8 py-8 rounded-full font-semibold shadow-md hover:bg-gray-800 transition duration-200">
           GET FREE ACCOUNT
         </Button>
-        <Button className="bg-yellow-500 text-lg text-black px-4 md:px-8 py-6 rounded-full shadow-md hover:bg-yellow-600 transition duration-200">
+        <Button className="bg-yellow-500 text-lg text-black px-4 md:px-8 py-8 rounded-full font-semibold shadow-md hover:bg-yellow-600 transition duration-200">
           VIEW PRODUCT DEMO
         </Button>
       </div>
-      <p className="text-gray-500 text-sm pt-2">No credit card required</p>
+      <p className="text-gray-800 font-semibold text-md pt-1">No credit card required</p>
     </div>
   );
 }
